39cc1255db987b6d1438ab71dfc6125c8bd99f3d
wjinss <aastu4343@gmail.com>
2025-03-25 03:04:07 +0900

    헤더 스타일 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,69 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import "../styles/Header.css";
+import logo from "../assets/images/logo.svg";
 
 const Header = () => {
+  const [action, setAction] = useState(false);
+  const handleMMENUClick = () => {
+    setAction(!action);
+  };
+  const [gnbAction, setGnbAction] = useState(false);
+  const handleGNBClick = (id) => {
+    setGnbAction(id);
+  };
+  //   상태로 클래스 변경하기
   return (
-    <header>
-      <h1>
-        <Link to={"/"}>
-          <img src="" alt="" />
-        </Link>
-      </h1>
-      <div className="locaAndUser">
-        <button></button>
-        <button></button>
-      </div>
-      <div className="mobile_menu">
-        <strong className="blind">메뉴 열기</strong>
-        <button type="button" className="mmenu">
-          <span className="buger"></span>
-          <span className="buger"></span>
-          <span className="buger"></span>
-        </button>
-      </div>
-      <div className="searchAndWish">
-        <button></button>
-        <button></button>
-      </div>
-      <nav className="nav">
-        <strong className="blind">메인 메뉴</strong>
-        <ul className="gnb">
-          <li className="d1">
-            <Link to={"/product"}>제품보기</Link>
-            <div className="pro_gnb_bg">
-              <div className="pro_gnb_in">
+    <header id="Header">
+      <div className="in header_in">
+        <h1 className="logo">
+          <Link to={"/"}>
+            <img src={logo} alt="라이카 로고" />
+          </Link>
+        </h1>
+        <div className="locaAndUser">
+          <button type="button">
+            <span></span>
+          </button>
+          <button type="button">
+            <span></span>
+          </button>
+        </div>
+        <div className="mobile_menu">
+          <strong className="blind">메뉴 열기</strong>
+          <button
+            type="button"
+            className={`mmenu ${action ? "active" : ""}`}
+            onClick={handleMMENUClick}
+          >
+            <span className="buger"></span>
+            <span className="buger"></span>
+            <span className="buger"></span>
+          </button>
+        </div>
+        <div className="searchAndWish">
+          <button type="button">
+            <span></span>
+          </button>
+          <button type="button">
+            <span></span>
+          </button>
+        </div>
+        <nav className={`nav ${action ? "active" : ""}`}>
+          <strong className="blind">메인 메뉴</strong>
+          <ul className="gnb">
+            <li className="d1">
+              <Link
+                to={"/product"}
+                onClick={() => handleGNBClick("product")}
+                className={`gnb_link ${
+                  gnbAction === "product" ? "active" : ""
+                }`}
+                //   상태로 클래스 변경하기
+              >
+                제품보기
+              </Link>
+              <div className="gnb_bg pro_gnb_bg">
                 <ul className="pro_lnb">
                   <li className="pro_d2">
                     <Link to={"/product"}>
@@ -40,7 +73,7 @@ const Header = () => {
                         <dd>시그니처 레인지파인더 카메라</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>Leica M11</Link>
                       </li>
@@ -66,7 +99,7 @@ const Header = () => {
                         <dd>하이브리드 카메라</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>Leica SL3</Link>
                       </li>
@@ -89,7 +122,7 @@ const Header = () => {
                         <dd>하이엔드 풀프레임 카메라</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>Leica Q3</Link>
                       </li>
@@ -112,7 +145,7 @@ const Header = () => {
                         <dd>엔트리 카메라</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>D-Lux8</Link>
                       </li>
@@ -135,7 +168,7 @@ const Header = () => {
                         <dd>하이브리드 즉석카메라</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>Sofort</Link>
                       </li>
@@ -152,7 +185,7 @@ const Header = () => {
                         <dd>시간을 포착하다</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>Leica Watch</Link>
                       </li>
@@ -166,7 +199,7 @@ const Header = () => {
                         <dd>컴팩트 쌍안경</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>ULTRAVID</Link>
                       </li>
@@ -192,9 +225,11 @@ const Header = () => {
                         <dd>원스톱 서비스</dd>
                       </dl>
                     </Link>
-                    <ul>
+                    <ul className="lnb">
                       <li>
                         <Link to={"/product"}>매입 안내</Link>
+                      </li>
+                      <li>
                         <Link to={"/product"}>판매 제품</Link>
                       </li>
                     </ul>
@@ -209,72 +244,110 @@ const Header = () => {
                   </Link>
                 </div>
               </div>
-            </div>
-          </li>
-          <li className="d1">
-            <Link to={"/second-hand"} className="gnb_link">
-              세컨드핸드
-            </Link>
-            <div className="gnb_bg">
-              <div className="gnb_in">
+            </li>
+            <li className="d1">
+              <Link
+                to={"/second-hand"}
+                onClick={() => handleGNBClick("second-hand")}
+                className={`gnb_link ${
+                  gnbAction === "second-hand" ? "active" : ""
+                }`}
+              >
+                세컨드핸드
+              </Link>
+              <div className="gnb_bg">
                 <ul className="lnb">
                   <li className="d2">
                     <Link to={"/second-hand"}>매입 안내</Link>
+                  </li>
+                  <li>
                     <Link to={"/second-hand"}>제품 목록</Link>
                   </li>
                 </ul>
               </div>
-            </div>
-          </li>
-          <li className="d1">
-            <Link to={"/world"}>라이카월드</Link>
-            <div className="gnb_bg">
-              <div className="gnb_in">
+            </li>
+            <li className="d1">
+              <Link
+                to={"/world"}
+                onClick={() => handleGNBClick("world")}
+                className={`gnb_link ${gnbAction === "world" ? "active" : ""}`}
+              >
+                라이카월드
+              </Link>
+              <div className="gnb_bg">
                 <ul className="lnb">
                   <li className="d2">
                     <Link to={"/world"}>라이카 전시</Link>
+                  </li>
+                  <li>
                     <Link to={"/world"}>온라인 갤러리</Link>
+                  </li>
+                  <li>
                     <Link to={"/world"}>라이카 스토리</Link>
+                  </li>
+                  <li>
                     <Link to={"/world"}>브랜드 소개</Link>
                   </li>
                 </ul>
               </div>
-            </div>
-          </li>
-          <li className="d1">
-            <Link to={"/akademie"}>아카데미</Link>
-            <div className="gnb_bg">
-              <div className="gnb_in">
+            </li>
+            <li className="d1">
+              <Link
+                to={"/akademie"}
+                onClick={() => handleGNBClick("akademie")}
+                className={`gnb_link ${
+                  gnbAction === "akademie" ? "active" : ""
+                }`}
+              >
+                아카데미
+              </Link>
+              <div className="gnb_bg">
                 <ul className="lnb">
                   <li className="d2">
                     <Link to={"/akademie"}>라이카 아카데미 소개</Link>
+                  </li>
+                  <li>
                     <Link to={"/akademie"}>아카데미 일정</Link>
                   </li>
                 </ul>
               </div>
-            </div>
-          </li>
-          <li className="d1">
-            <Link to={"/customer"}>고객지원</Link>
-            <div className="gnb_bg">
-              <div className="gnb_in">
+            </li>
+            <li className="d1">
+              <Link
+                to={"/customer"}
+                onClick={() => handleGNBClick("customer")}
+                className={`gnb_link ${
+                  gnbAction === "customer" ? "active" : ""
+                }`}
+              >
+                고객지원
+              </Link>
+              <div className="gnb_bg">
                 <ul className="lnb">
                   <li className="d2">
-                    <Link to={"/customer"}>공지사항</Link>
-                    <Link to={"/customer"}>제품등록</Link>
-                    <Link to={"/customer"}>매장 방문 예약</Link>
-                    <Link to={"/customer"}>다운로드</Link>
-                    <Link to={"/customer"}>자주 하는 질문(FAQ)</Link>
+                    <Link>공지사항</Link>
+                  </li>
+                  <li>
+                    <Link>제품등록</Link>
+                  </li>
+                  <li>
+                    <Link>매장 방문 예약</Link>
+                  </li>
+                  <li>
+                    <Link>다운로드</Link>
+                  </li>
+                  <li>
+                    <Link>자주 하는 질문(FAQ)</Link>
                   </li>
                 </ul>
               </div>
-            </div>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <Link to={"/"}>제품등록</Link>
-        <Link to={"/"}>PRO-MEMBERSHIP</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* <div>
+          <Link to={"/"}>제품등록</Link>
+          <Link to={"/"}>PRO-MEMBERSHIP</Link>
+        </div> */}
       </div>
     </header>
   );
